64b6c050ecf5f96cf10cffbdb522e02454dea454
Cat <ziyuye@uchicago.edu>
2024-01-16 12:55:08 -0600

    typo

--- main_debug.py
+++ main_debug.py
@@ -13,7 +13,7 @@ import pprint
 import yaml
 
 from src.utils.distributed import init_distributed
-from src.train_debug import main as app_main
+from src.debug import main as app_main
 
 parser = argparse.ArgumentParser()
 parser.add_argument(
